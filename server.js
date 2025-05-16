cd3d68f6ca45b1fee80311e75b05cbb47846a9c1
jsdgbernardo <bernardojonathansimon@gmail.com>
2025-05-17 00:12:42 +0800

    server js changes

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const port = process.env.PORT || 3000;
 app.use(cors()); // allow frontend calls
 app.use(express.json());
 
-let latestData = null;
+let latestWeight = null;  // Store the most recent data
 
 app.post('/api/data', (req, res) => {
-  latestData = req.body;
-  console.log('Received:', latestData);
-  res.status(200).send('Data received');
+  const { weight } = req.body;
+  console.log('Received weight:', weight);
+  latestWeight = weight;
+  res.send('Data received');
 });
 
 app.get('/api/data', (req, res) => {
-  res.json(latestData || { message: "No data yet" });
+  res.json({ weight: latestWeight });
 });
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
